80141a80667cd08126464fc19f819148db82633f
Ralph van Schaik <ralph.van.schaik@me.com>
2020-11-15 12:35:21 +0100

    Tm 158 selecting Sibling elements

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ h1.textContent = 'Some new title';
 h1.style.color = 'white';
 h1.style.backgroundColor = 'black';
 
-const li = document.querySelector('li:nth-of-type(2)');
+const li = document.querySelector('li:last-of-type');
 // console.dir(li);
 console.log(li.textContent);
 li.textContent = li.textContent + ' (Changed)';
